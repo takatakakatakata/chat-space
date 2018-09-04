88da0e3e64d90f652a5acb696f962a025e53bcee
takahirohayasaka <tkhr8964@gmail.com>
2018-09-04 14:18:52 +0900

    Add _login_logout_edit_function

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,9 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
 
-  before_action :configure_permitted_parameter if devise_controller?
+  before_action :configure_permitted_parameter ,if: :devise_controller?
 
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up,keys: [:name])
+  def configure_permitted_parameter
+     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
 end
