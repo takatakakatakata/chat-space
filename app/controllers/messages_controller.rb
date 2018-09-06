74c8bada029727b21552d2b40ccb3261bf5b933c
takahirohayasaka <tkhr8964@gmail.com>
2018-09-06 14:40:13 +0900

    Change messages_view

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,9 +1,24 @@
 class MessagesController < ApplicationController
+  before_action :set_group
   def index
+    @message = Message.new
+    @messages = @group.messages.includes(:user)
   end
   def create
+    @message = @group.messages.new(messages_params)
+    if @message.save
+      redirect_to group_messages_path(@group) ,notice:"メッセージを作成されました"
+    else
+      @messages = @group.messages.includes(:user) 
+      flash.now[:alert] = 'メッセージを入力してください'
+      render :index
+    end
   end
   private
   def messages_params
+    params.require(:message).permit(:body,:image).merge(user_id: current_user.id)
+  end
+  def set_group
+    @group = Group.find(params[:group_id])
   end
 end
