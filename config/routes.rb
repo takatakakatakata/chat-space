5832d99f49fd1f173128f992391a1258149703d6
takahirohayasaka <tkhr8964@gmail.com>
2018-09-06 14:39:04 +0900

    Set model_asosiations

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   root 'groups#index'
   resources :users ,only:[:edit,:update]
   resources :groups,only:[:new,:create,:edit,:update] do
-    resources :messages, only:[:index,:create]
+    resources :messages, only:[:index, :create]
   end
 
 end
