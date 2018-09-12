b5a07b2a750285d6ecf7972e8a7e7225da4976af
takahirohayasaka <tkhr8964@gmail.com>
2018-09-12 14:48:37 +0900

    edit incremental_search_function

---
+++ app/assets/javascripts/user.js
@@ -0,0 +1,57 @@
+$(function(){
+  // インクリメンタルサーチ
+  function build_userlist(user){
+    var html = `<div class="chat-group-user clearfix">
+                  <p class="chat-group-user__name">${ user.name }</p>
+                  <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${ user.id}" data-user-name="${ user.name }">追加</a>
+                </div>`
+    return html;
+  }
+
+  function add_userlist(user_id, user_name){
+    var html = `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-8'>
+                  <input name='group[user_ids][]' type='hidden' value='${ user_id }'>
+                  <p class='chat-group-user__name'>${ user_name }</p>
+                  <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
+                </div>`
+    return html;
+  }
+
+  $('#user-search-field').on('keyup',function(){
+    $("#user-search-result").empty();
+    var input = $('#user-search-field').val();
+    $.ajax({
+      type:"GET",
+      data: {keyword: input},
+      dataType: 'json',
+      url: "/users"
+    })
+
+    .done(function(users){
+      users.forEach(function(user){
+      $('#user-search-result').append(build_userlist(user));
+      });
+    })
+    .fail(function(){
+      alert("検索に失敗しました");
+    });
+  });
+
+
+//追加機能
+  $(document).on("click", ".user-search-add.chat-group-user__btn.chat-group-user__btn--add", function(){
+    var user_name = $(this).attr('data-user-name');
+    var user_id = $(this).data('user-id');
+    var user_list = add_userlist(user_id, user_name);
+    $('#chat-group-users').append(user_list);
+    $(this).parent().remove();
+  });
+
+//削除機能
+ $(document).on("click",".user-search-remove.chat-group-user__btn.chat-group-user__btn--remove.js-remove-btn",function(){
+   $(this).parent().remove();
+   a=$(this).parent();
+   console.log(a)
+ });
+
+});
